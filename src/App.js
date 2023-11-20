525bb38c99812e4f239771b90a4cb18d1cfe7a97
hijunglit <hijunglit@gmail.com>
2023-11-20 11:44:17 +0900

    styled component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Father = styled.div`
+  display: flex;
+`
+const Box1 = styled.div`
+  background: tomato;
+  width: 100px;
+  height: 100px;
+`
+const Box2 = styled.div`
+  background: orange;
+  width: 100px;
+  height: 100px;
+`
+const Span = styled.span`
+  color: #fff;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Father>
+      <Box1><Span>Hi!</Span></Box1>
+      <Box2></Box2>
+    </Father>
+  )
 }
 
 export default App;
