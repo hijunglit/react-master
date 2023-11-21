db3384424d5743a1a40720f9241418b90bb0f75b
hijunglit <hijunglit@gmail.com>
2023-11-21 16:55:56 +0900

    forms with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import Circle from "./Circle.tsx";
+import React, { useState } from 'react'
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  }
+  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
   return (
     <div>
-      <Circle borderColor="teal"/>
-      <Circle bgColor="tomato"/>
+      <form onSubmit={onsubmit}>
+        <input 
+          value={value}
+          onChange={onchange}
+          type="text" 
+          placeholder='username'
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
