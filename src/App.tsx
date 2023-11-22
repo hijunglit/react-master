44585bc09311dc3180ffaeb4b0f15af634b89bd1
hijunglit <hijunglit@gmail.com>
2023-11-22 15:43:26 +0900

    typescript with theme of style-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,6 @@
-import React, { useState } from 'react'
-
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  }
-  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
-  return (
-    <div>
-      <form onSubmit={onsubmit}>
-        <input 
-          value={value}
-          onChange={onchange}
-          type="text" 
-          placeholder='username'
-        />
-        <button>Log in</button>
-      </form>
-    </div>
-  );
+  
+  return null;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import App from './App.tsx';
+import { darkTheme, lightTheme } from "./theme.ts";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
