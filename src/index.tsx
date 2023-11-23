6412300ed2318e9e6efe09a6219de3b7e0e9c08f
hijunglit <hijunglit@gmail.com>
2023-11-23 11:07:31 +0900

    crypto tracker with typescript & react

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import App from './App.tsx';
-import { darkTheme, lightTheme } from "./theme.ts";
+import { theme } from "./theme.ts";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
