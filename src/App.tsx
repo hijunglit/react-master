7a11e111cbb6eedcf0029cc57346e98d3253b0af
hijunglit <hijunglit@gmail.com>
2023-12-06 21:30:05 +0900

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,51 @@
+import styled, { keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+`;
+
+const rotationAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius:0px;
+  }
+  50% {
+    border-radius:100px;
+  }
+  100% {
+    transform:rotate(360deg);
+    border-radius:0px;
+  }
+`;
+
+const Emoji = styled.span`
+  font-size: 36px; 
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 1s linear infinite;
+  ${Emoji}:hover {
+    font-size: 98px;
+  }
+`;
+
 function App() {
-  return null;
+  return (
+    <Wrapper>
+      <Box>
+        <Emoji>🤪</Emoji>
+      </Box>
+    </Wrapper>
+  );
 }
 
 export default App;
