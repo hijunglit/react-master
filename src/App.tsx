0809ebca4136a14e4f6bccc15bab5d7e68d14fc0
hijunglit <hijunglit@gmail.com>
2023-12-06 20:27:01 +0900

    init for review

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
-import { createGlobalStyle } from 'styled-components';
-import ToDoList from "./components/ToDoList.tsx";
-
-const GlobalStyle = createGlobalStyle ``;
-
 function App() {
-  return (
-    <>
-      <GlobalStyle />
-      <ToDoList />
-    </>
-  );
+  return null;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RecoilRoot } from 'recoil';
-import { ThemeProvider } from 'styled-components';
 import App from './App.tsx';
-import { darkTheme } from "./theme.ts"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-      <RecoilRoot>
-        <ThemeProvider theme={darkTheme}>
-          <App />
-        </ThemeProvider>
-      </RecoilRoot>
+    <App />
   </React.StrictMode>
 )
