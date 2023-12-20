caa8fe33db5ed8eeaff063ad447774c1f3bda03f
hijunglit <hijunglit@gmail.com>
2023-12-20 17:20:52 +0900

    init for assignment && coin tracker(nested routes for coin detail)

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,5 +1,9 @@
-import "styled-components";
+import { DefaultTheme } from 'styled-components';
 
 declare module "styled-components" {
-  export interface DefaultTheme {}
+  export interface DefaultTheme {
+    textColor: string;
+    bgColor: string;
+    accentColor: string;
+  }
 }
